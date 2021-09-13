89ca5299f5eb6226f6cb9dbe4618ab06d105470e
IsaacTseng <isaac.kuang@gmail.com>
2021-09-13 10:21:52 +0800

    fix........

--- add-s6-overlay.sh
+++ add-s6-overlay.sh
@@ -12,6 +12,7 @@ case $arch in
     ;;
     'aarch64' )
     wget -O s6-overlay-installer https://github.com/just-containers/s6-overlay/releases/download/${version}/s6-overlay-aarch64-installer
+    ;;
     *)
     echo "Arch Unknow"
     exit 1
